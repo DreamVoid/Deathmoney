4ed349469c6e7b6488908821e9070d91f4953654
流光溢彩Last <i@dreamvoid.me>
2023-07-22 18:36:46 +0800

    添加快速复活

--- src/main/java/org/mineblock/deathtweak/listener/PlayerDeathListener.java
+++ src/main/java/org/mineblock/deathtweak/listener/PlayerDeathListener.java
@@ -1,5 +1,6 @@
 package org.mineblock.deathtweak.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -14,39 +15,43 @@ import java.text.DecimalFormat;
 
 public class PlayerDeathListener implements Listener {
 
-	@EventHandler
-	public void onEntityDeath(PlayerDeathEvent event) {
-		Player player = event.getEntity().getPlayer();
+    @EventHandler
+    public void onEntityDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity().getPlayer();
 
-		if(MineBlockDeathTweak.INSTANCE.getConfig().getBoolean("player.lost-money", true)) {
-			// 丢钱
-			if (MineBlockDeathTweak.economy != null) {
-				double money = MineBlockDeathTweak.economy.getBalance(player);
-				if (money >= 0.01) {
-					DecimalFormat df = new DecimalFormat("#,###,##0.00");
-					double lost = Math.min(money * 0.5 >= 0.01 ? money * 0.5 : money, 5000000);
-					MineBlockDeathTweak.economy.withdrawPlayer(player, lost);
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c你因为死亡损失了" + df.format(lost) + "硬币！"));
-				}
+        if(MineBlockDeathTweak.INSTANCE.getConfig().getBoolean("player.lost-money", true)) {
+            // 丢钱
+            if (MineBlockDeathTweak.economy != null) {
+                double money = MineBlockDeathTweak.economy.getBalance(player);
+                if (money >= 0.01) {
+                    DecimalFormat df = new DecimalFormat("#,###,##0.00");
+                    double lost = Math.min(money * 0.5 >= 0.01 ? money * 0.5 : money, 5000000);
+                    MineBlockDeathTweak.economy.withdrawPlayer(player, lost);
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c你因为死亡损失了" + df.format(lost) + "硬币！"));
+                }
 
-				if(player.getLevel() >= 1){
-					int lossLevel = player.getLevel() / 2;
-					player.setLevel(player.getLevel() - lossLevel);
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c你因为死亡损失了" + lossLevel + "级经验！"));
-				}
-			}
-		}
+                if(player.getLevel() >= 1){
+                    int lossLevel = player.getLevel() / 2;
+                    player.setLevel(player.getLevel() - lossLevel);
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c你因为死亡损失了" + lossLevel + "级经验！"));
+                }
+            }
+        }
 
-		if(MineBlockDeathTweak.INSTANCE.getConfig().getBoolean("player.lost-vanishing-curse-item", true)) {
-			// 丢消失诅咒装备
-			ItemStack air = new ItemStack(Material.AIR);
-			for (int i = 0; i < player.getInventory().getSize(); i++) {
-				ItemStack item = player.getInventory().getItem(i);
-				if (item != null && item.containsEnchantment(Enchantment.VANISHING_CURSE)) {
-					player.getInventory().setItem(i, air);
-				}
-			}
-			player.updateInventory();
-		}
-	}
+        if(MineBlockDeathTweak.INSTANCE.getConfig().getBoolean("player.lost-vanishing-curse-item", true)) {
+            // 丢消失诅咒装备
+            ItemStack air = new ItemStack(Material.AIR);
+            for (int i = 0; i < player.getInventory().getSize(); i++) {
+                ItemStack item = player.getInventory().getItem(i);
+                if (item != null && item.containsEnchantment(Enchantment.VANISHING_CURSE)) {
+                    player.getInventory().setItem(i, air);
+                }
+            }
+            player.updateInventory();
+        }
+
+        if(player.hasPermission("mineblock.allow.instantrespawn")){
+            Bukkit.getScheduler().runTaskLater(MineBlockDeathTweak.INSTANCE, () -> player.spigot().respawn(), 2);
+        }
+    }
 }
